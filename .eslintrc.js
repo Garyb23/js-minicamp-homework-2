c6eff2724271516e9b2e40f0f81a6e1a41c04641
Gabe Mulford <gabe@todaywebpage.com>
2017-03-15 13:45:43 -0700

    Finished my homework

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
             "error",
             "single"
         ],
-        "no-console": ["error", { "allow": ["warn", "error"] }],
         "no-unused-vars": ["error", { "vars": "all", "args": "none" }],
         "semi": [
             "error",
